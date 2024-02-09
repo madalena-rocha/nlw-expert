82a2ed7ad1ddddf7c39f0d590240e66bd89160e3
madalena-rocha <rochamada1997m@gmail.com>
2024-02-09 16:23:21 -0300

    create modals

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,12 +1,44 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { X } from 'lucide-react'
+
 export function NewNoteCard() {
   return (
-    <div className="rounded-md bg-slate-700 p-5 space-y-3">
-      <span className="text-sm font-medium text-slate-200">
-        Adicionar nota
-      </span>
-      <p className="text-sm leading-6 text-slate-400">
-        Grave uma nota em áudio que será convertida para texto automaticamente.
-      </p>
-    </div>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 text-left p-5 gap-3 outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-200">
+          Adicionar nota
+        </span>
+        <p className="text-sm leading-6 text-slate-400">
+          Grave uma nota em áudio que será convertida para texto automaticamente.
+        </p>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50">
+          <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+            <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+              <X className="size-5" />
+            </Dialog.Close>
+            
+            <div className="flex flex-1 flex-col gap-3 p-5">
+              <span className="text-sm font-medium text-slate-300">
+                Adicionar nota
+              </span>
+
+              <p className="text-sm leading-6 text-slate-400">
+                Comece <button className="font-medium text-lime-400 hover:underline">gravando uma nota</button> em áudio ou se preferir <button className="font-medium text-lime-400 hover:underline">utilize apenas texto</button>.
+              </p>
+            </div>
+
+            <button
+              type="button"
+              className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+            >
+              Salvar nota
+            </button>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,3 +1,9 @@
+import * as Dialog from '@radix-ui/react-dialog'
+// O * pega todas as exportações que o react-dialog faz e coloca dentro de um objeto chamado Dialog
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { X } from 'lucide-react'
+
 interface NoteCardProps {
   note: {
     date: Date
@@ -7,16 +13,63 @@ interface NoteCardProps {
 
 export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-      {/* O focus aplica o CSS ao clicar no elemento, já o focus-visible aplica o CSS ao navegar na aplicação pelo teclado */}
-      <span className="text-sm font-medium text-slate-300">
-        {note.date.toISOString()}
-      </span>
-      <p className="text-sm leading-6 text-slate-400">
-        {note.content}
-      </p>
+    <Dialog.Root>
+      {/* O Dialog.Trigger é usado no elemento que abre o modal */}
+      <Dialog.Trigger className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        {/* O focus aplica o CSS ao clicar no elemento, já o focus-visible aplica o CSS ao navegar na aplicação pelo teclado */}
+        <span className="text-sm font-medium text-slate-300">
+          {note.date.toISOString()}
+        </span>
+        <p className="text-sm leading-6 text-slate-400">
+          {note.content}
+        </p>
 
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </button>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+      </Dialog.Trigger>
+
+      {/* 
+        O Dialog.Portal faz com que o conteúdo dentro dele não seja mostrado no local onde está sendo colocado 
+        Ele teleporta o conteúdo para a raiz da aplicação
+      */}
+      <Dialog.Portal>
+        {/* O Dialog.Overlay é a div que cobre a aplicação toda */}
+        <Dialog.Overlay className="inset-0 fixed bg-black/50">
+          {/* O Dialog.Content é o que deve aparecer ao clicar no modal */}
+          <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+            {/* O Dialog.Close é um botão de fechar */}
+            <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+              <X className="size-5" />
+            </Dialog.Close>
+            
+            <div className="flex flex-1 flex-col gap-3 p-5">
+              {/* 
+                O flex-grow indica se o elemento pode crescer ou não 
+                O flex-shrink indica se o elemento pode reduzir o seu tamanho ou não baseado no tamanho dos seus irmãos
+                O flex-basis é o tamanho base do elemento, ou seja, o tamanho que ele deve seguir caso não cresça nem diminua
+                O flex-1 faz com que o elemento ocupe o máximo de espaço possível, mas se houver outros elementos que precisem daquele espaço, ele pode reduzir o seu tamanho
+              */}
+              <span className="text-sm font-medium text-slate-300">
+                {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true})}
+              </span>
+
+              <p className="text-sm leading-6 text-slate-400">
+                {note.content}
+              </p>
+            </div>
+
+            <button
+              type="button"
+              className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
+            >
+              Deseja <span className="text-red-400 group-hover:underline">apagar essa nota</span>?
+              {/* 
+                Para aplicar um estilo em um elemento baseado em um seletor do pai, utilizar a classe group no elemento pai
+                Quando o grupo, que é o elemento pai onde foi colocada a classe group, estiver com o hover, colocar o underline no elemento filho
+              */}
+            </button>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
