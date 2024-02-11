fe1025a264e17fa9bda94d96a5a20d861faf1af9
madalena-rocha <rochamada1997m@gmail.com>
2024-02-11 17:12:32 -0300

    use local storage

--- src/app.tsx
+++ src/app.tsx
@@ -10,7 +10,15 @@ interface Note {
 }
 
 export function App() {
-  const [notes, setNotes] = useState<Note[]>([])
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notes')
+
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage)
+    }
+
+    return []
+  })
 
   function onNoteCreated(content: string) {
     const newNote = {
@@ -19,7 +27,11 @@ export function App() {
       content,
     }
 
-    setNotes([newNote, ...notes])
+    const notesArray = [newNote, ...notes]
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
   return (
